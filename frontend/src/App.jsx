c63d5dbaf96b229f81606b8873b1b1e30839d80a
Paulo Campos Filho <pezaum@gmail.com>
2023-07-13 14:56:19 -0700

    Added HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import PhotoList from './components/PhotoList';
-import TopicListItem from './components/TopicListItem';
 import './App.scss';
-import TopicList from './components/TopicList';
-import TopNavigationBar from './components/TopNavigationBar';
+import HomeRoute from './components/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    {/* <PhotoListItem/> */}
-    {/* <TopicListItem /> */}
-    {/* <TopicList />
-    <PhotoList /> */}
-    <TopNavigationBar/>
+    <HomeRoute/>
   </div>
 )
 
